8367a3b11e8d92bc18eb822695388766d394a3b3
Corey Levine <coreylevine2000@gmail.com>
2022-05-08 22:15:06 -0400

    templates for positons

--- Developed/application.js
+++ Developed/application.js
@@ -1,2 +1,103 @@
+//Require consts
 const inquirer = require("inquirer");
 const fs = require("fs");
+const path = require("path");
+
+//Questionair for inquirer
+const questions = [
+    {
+      type: "list",
+      message: "What is your role?",
+      name: "role",
+      choices: ["Engineer", "Intern", "Manager"],
+    },
+    {
+      type: "input",
+      message: "What is your name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is your ID?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is your email?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "github",
+      when: (response) => response.role === "Engineer",
+    },
+    {
+      type: "input",
+      message: "What school did you attend?",
+      name: "school",
+      when: (response) => response.role === "Intern",
+    },
+    {
+      type: "input",
+      message: "What is your office number?",
+      name: "officeNumber",
+      when: (response) => response.role === "Manager",
+    },
+    {
+      type: "list",
+      message: "Do you want to add another employee?",
+      name: "add",
+      choices: ["Yes", "No"],
+    },
+  ];
+  
+  //Inquirer init
+  const init = () => inquirer
+    .prompt(questions)
+    //Role selected creates new object specific to role
+    .then(createTeam);
+  
+  
+  //Function that adds all team info to specific employee
+  function createTeam(response) {
+    let employee;
+    if (response.role === "Engineer") {
+      employee = new Engineer(
+        response.name,
+        response.id,
+        response.email,
+        response.github
+      );
+    } else if (response.role === "Intern") {
+      employee = new Manager(
+        response.name,
+        response.id,
+        response.email,
+        response.school
+      );
+    } else if (response.role === "Manager") {
+      employee = new Intern(
+        response.name,
+        response.id,
+        response.email,
+        response.officeNumber
+      );
+    }
+    //Blank array pushed with employee responses
+    employees.push(employee);
+  
+    if (response.add === "Yes") {
+      init();
+    } else {
+      console.log("Finished");
+    }
+    fs.writeFile(outputPath, render(employees), function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  }
+  init();
+  
+  
