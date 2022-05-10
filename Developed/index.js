45dea608819e3409b49a01d82caa68e4e9b6fb3f
Corey Levine <coreylevine2000@gmail.com>
2022-05-09 21:37:59 -0400

    notes for tracking

--- Developed/index.js
+++ Developed/index.js
@@ -3,6 +3,18 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
 
+//Positions
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+//Outcome files
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+const employees = [];
+
 //Questionair for inquirer
 const questions = [
     {
